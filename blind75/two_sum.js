8c64715904f1cb3db974910bb8aca34274772aa1
Richard Miao <richardmiao1645@gmail.com>
2023-05-07 16:35:30 -0700

    Refactor and complete twoSum

--- blind75/two_sum.js
+++ blind75/two_sum.js
@@ -5,16 +5,23 @@
 
   You can return the answer in any order.
 
+  There may be duplicates of numbers. Return only the indexes of the first instance that each number appears. 
+
+  Do not trim or make any changes to the original array.
+
 Examples:
-  
-  Input: nums = [3,3], target = 6
-  Output: [0,1]
 
   Input: nums = [3,2,4], target = 6
   Output: [1,2]
 
-  Input: nums = [2,7,11,15], target = 9
-  Output: [0,1]
+  Input: nums = [15,2,7,11], target = 9
+  Output: [1,2]
+
+  Input: nums = [3,2,2,2,7,11,15], target = 9
+  Output: [1,4]
+
+  Input: nums = [3,3,4,2,2,4,4,4], target = 6
+  Output: [2,3]
 */
 
 /**
@@ -22,12 +29,22 @@ Examples:
  * @param {number} target
  * @return {number[]}
  */
+
+
 const twoSum = function(nums, target) {
 
   //Saves values that are not the needed values in an object
   const previousValues = {};
+  const uniqueNums = [];
 
   for (let i = 0; i < nums.length; i++) {
+
+    //Checks if the current number already has had occurences. Add it if it hasn't
+    if (uniqueNums.includes(nums[i])) {
+      continue;
+    } else {
+      uniqueNums.push(nums[i]);
+    }
 
     //Stores the current number and the needed number in constants.
     const currentNum = nums[i];
@@ -47,5 +64,7 @@ const twoSum = function(nums, target) {
   }
 };
 
-console.log(twoSum([2,7,11,15], 9)); //Output: [0,1]
-console.log(twoSum([3,2,4], 6)); //Output: [1,2]
+console.log(twoSum([15,2,7,11], 9)); //1,2
+console.log(twoSum([3,2,4], 6)); //1,2
+console.log(twoSum([3,2,2,2,7,11,15], 9)); //1,4
+console.log(twoSum([4,3,4,3,2,2,4,4,4], 6)); //0,4
