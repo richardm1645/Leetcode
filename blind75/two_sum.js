6697fed8bcf16428fe1faad50e79d54adac3f951
Richard Miao <richardmiao1645@gmail.com>
2023-05-07 16:02:06 -0700

    Lint and comment solution

--- blind75/two_sum.js
+++ blind75/two_sum.js
@@ -29,16 +29,23 @@ const twoSum = function(nums, target) {
 
   for (let i = 0; i < nums.length; i++) {
 
-    console.log(previousValues)
+    //Stores the current number and the needed number in constants.
     const currentNum = nums[i];
     const neededValue = target - currentNum;
-    const index2 = previousValues[neededValue]
+
+    //Attempts to find the key-value pair of the needed num in the object
+    const index2 = previousValues[neededValue];
+
+    //If index2 is not null and is not of the same type as null, return the current number and index2 as the answer.
     if (index2 != null) {
       return [index2, i];
+    
     } else {
-      previousValues[currentNum] = i
+      //If a solution is not found, stores the index of the current number in the array in the object
+      previousValues[currentNum] = i;
     }
   }
 };
 
-console.log(twoSum([2,7,11,15], 9))
+console.log(twoSum([2,7,11,15], 9)); //Output: [0,1]
+console.log(twoSum([3,2,4], 6)); //Output: [1,2]
